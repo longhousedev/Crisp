86d2922fec16d17406f59302025254c7b309dd71
George <george.xp7100@gmail.com>
2019-06-15 15:51:31 +0100

    Added arithmatic evaluation

--- prompt.c
+++ prompt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "mpc.h"
 
 //Windows functions
@@ -23,6 +24,39 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+//Performs arithmatic functions
+long evaluate_operation(long num, char *operator, long num2) {
+    if (strcmp(operator, "+") == 0) return num + num2;
+    if (strcmp(operator, "-") == 0) return num - num2;
+    if (strcmp(operator, "*") == 0) return num * num2;
+    if (strcmp(operator, "/") == 0) return num / num2;
+    if (strcmp(operator, "%") == 0) return num % num2;
+    if (strcmp(operator, "^") == 0) return pow(num, num2);
+}
+
+long evaluate(mpc_ast_t* tree) {
+
+    //Base Case
+    if(strstr(tree->tag, "number")) {
+        return atoi(tree->contents);
+    }
+
+    //Gets the operator from subprocedure
+    char* operator = tree->children[1]->contents;
+
+    //Gets the first number used in subprocedure
+    long num = evaluate(tree->children[2]);
+
+    //Go through the children of the tree and perform operations on returned value
+    int i = 3;
+    while (strstr(tree->children[i]->tag, "expression")) {
+        num = evaluate_operation(num, operator, evaluate(tree->children[i]));
+        i++;
+    }    
+
+    return num;
+}
+
 int main(int argc, char** argv) {
     
     //Notation
@@ -35,23 +69,29 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
     "                        \
         number : /-?[0.0-9]+/; \
-        operator : '+' | '-' | '/' | '*' | '%'; \
+        operator : '+' | '-' | '/' | '*' | '%' | '^'; \
         expression : <number> | '(' <operator> <expression>+ ')'; \
         crisp: /^/ <operator> <expression>+ /$/; \
     ", Number, Operator, Expression, Crisp);
 
 
     //Information
-    puts("Crisp v0.0.2");
+    puts("Crisp v0.1.0");
     puts("Press Ctrl+c to exit \n");
 
     //Main loop
+    int debug = 0;
     while (1) {
         char* input =readline("crisp> ");
         add_history(input);
         //Attempt to parse
         mpc_result_t r;
-        if (mpc_parse("<stdin>", input, Crisp, &r)) {
+        if (mpc_parse("<stdin>", input, Crisp, &r) && debug == 0) {
+            long result = evaluate(r.output);
+            printf("%li\n", result);
+            mpc_ast_delete(r.output);
+        }
+        else if (mpc_parse("<stdin>", input, Crisp, &r) && debug == 1) {
             mpc_ast_print(r.output);
             mpc_ast_delete(r.output);
         }
@@ -65,10 +105,4 @@ int main(int argc, char** argv) {
     //Cleanup
     mpc_cleanup(4, Number, Operator, Expression, Crisp);
     return 0;
-}
-
-long evaluation(mpc_ast_t* tree) {
-
-    //Base Case
-
 }
